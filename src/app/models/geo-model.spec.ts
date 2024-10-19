a15132a5e06c93cd986fc7ba8b22a570d36e440f
Doug Van Auken <douglasvanauken@gmail.com>
2024-10-19 07:35:50 -0500

    Init dev

--- src/app/models/geo-model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { GeoModel } from './geo-model';
-
-describe('GeoModel', () => {
-  it('should create an instance', () => {
-    expect(new GeoModel()).toBeTruthy();
-  });
-});
--- src/app/models/json.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { JSONModel } from './json.model';
-
-describe('JSONModel', () => {
-  it('should create an instance', () => {
-    expect(new JSONModel()).toBeTruthy();
-  });
-});
